b14da970b8adfdc4c02a35826496a82de9bb3fce
Progra2-2690-24-4072 <ltaracenam1@miumg.edu.gt>
2025-10-01 23:36:42 -0600

    Update Transaccion.java

--- ejercicio pagos/Transaccion.java
+++ ejercicio pagos/Transaccion.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ejemplo.pkg2;
 
-/**
- *
- * @author crist
- */
 public class Transaccion {
     protected double monto;
 
@@ -19,4 +12,5 @@ public class Transaccion {
         System.out.println("Procesando pago de: Q" + monto);
     }
 }
+
 
